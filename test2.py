5bfe718604368f188aec46614b62a78ba852785a
gegehennb <1121130406@qq.com>
2018-09-24 16:38:49 +0800

    练习2，3

--- test2.py
+++ test2.py
@@ -5,10 +5,15 @@ matrix2=tf.constant([[2],[2]])
 
 protduct=tf.matmul(matrix1,matrix2)     #矩阵乘法
 
-
+'''
 #形式一
 sess=tf.Session()
 result=sess.run(protduct)
 print(result)
 sess.close()
+'''
 
+#形式2
+with tf.Session() as sess:
+    result2=sess.run(product)
+    print(result2)
